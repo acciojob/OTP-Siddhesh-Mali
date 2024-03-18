52544aa591591fe549e0972263c75a94728a0d0b
Siddhesh-Mali <129844841+Siddhesh-Mali@users.noreply.github.com>
2024-03-19 01:23:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const codeInputs = document.querySelectorAll('.code');
+
+codeInputs.forEach((input, index) => {
+  input.addEventListener('keydown', (event) => {
+    if (event.key === 'Backspace' && input.value === '') {
+      if (index > 0) {
+        codeInputs[index - 1].focus();
+      }
+    } else if (event.key !== 'Backspace' && input.value !== '') {
+      if (index < codeInputs.length - 1) {
+        codeInputs[index + 1].focus();
+      }
+    }
+  });
+});
